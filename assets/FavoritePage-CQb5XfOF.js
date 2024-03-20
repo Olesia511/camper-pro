fc6fd3d37a3fd2e1831c18bc3f7979db4f36f02f
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-03-20 17:33:01 +0200

    Updates

---
+++ assets/FavoritePage-CQb5XfOF.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D-U9genK.js";function t(){return r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Favorite PAGE "}),r.jsx("h2",{children:"WELCOME"})]})}export{t as default};
