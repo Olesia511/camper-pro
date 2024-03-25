5a63b056e6414596b07b064551e63116b2cac730
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-03-25 08:26:20 +0200

    Updates

---
+++ assets/HomePage-C0M5BYeG.js
@@ -0,0 +1,9 @@
+import{u as e,j as o}from"./index-CXlHHzlU.js";const t="/camper-pro/back.webp",a=e.section`
+  height: 100vh;
+  width: 100vw;
+  background-image: url(${t});
+  background-repeat: no-repeat;
+  background-position: right bottom;
+  background-size: cover;
+  background-attachment: fixed;
+`;function n(){return o.jsx(a,{})}export{n as default};
--- assets/HomePage-CGGdieoo.js
+++
@@ -1,9 +0,0 @@
-import{u as e,j as t}from"./index-CGgw3pta.js";const o="/camper-pro/assets/back-C6Z5sdsg.webp",a=e.section`
-  height: 100vh;
-  width: 100vw;
-  background-image: url(${o});
-  background-repeat: no-repeat;
-  background-position: right bottom;
-  background-size: cover;
-  background-attachment: fixed;
-`;function n(){return t.jsx(a,{})}export{n as default};
