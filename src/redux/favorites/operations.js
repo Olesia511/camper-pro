95e87c59b8582ecc3f08d03ca2f145a547fd07da
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-03-25 06:27:21 +0200

    delete console.log

--- src/redux/favorites/operations.js
+++ src/redux/favorites/operations.js
@@ -4,7 +4,6 @@ import axios from "axios";
 export const addFavorites = createAsyncThunk("favorites/fetchById", async (id, thunkAPI) => {
   try {
     const response = await axios.get(`/adverts/${id}`);
-    console.log(`response.data`, response.data);
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
