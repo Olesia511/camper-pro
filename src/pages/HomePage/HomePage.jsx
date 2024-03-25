031487f611484cf804d8876ffa403d73ba8239a2
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-03-25 14:01:57 +0200

    styled home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,24 @@
-import { WelcomeSection } from "./HomePage.styled";
+import { ImgFirst, ImgWrapper, Shadow, Title, WelcomeSection } from "./HomePage.styled";
+
+import image2 from "../../assets/backg-img-2-min.jpg";
+import image3 from "../../assets/backg-img-3-min.jpg";
+import image4 from "../../assets/backg-img-4-min.jpg";
+import image5 from "../../assets/backg-img-5-min.jpg";
+import image6 from "../../assets/backg-img-6-min.jpg";
 
 export default function HomePage() {
-  return <WelcomeSection></WelcomeSection>;
+  return (
+    <WelcomeSection>
+      <Shadow>
+        <Title>Discover Easy Travel</Title>
+        <ImgWrapper>
+          <ImgFirst src={image2}></ImgFirst>
+          <ImgFirst src={image3}></ImgFirst>
+          <ImgFirst src={image4}></ImgFirst>
+          <ImgFirst src={image5}></ImgFirst>
+          <ImgFirst src={image6}></ImgFirst>
+        </ImgWrapper>
+      </Shadow>
+    </WelcomeSection>
+  );
 }
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,5 +1,5 @@
 // import image from "../../../public/back.webp";
-import image from "/back.webp";
+import image from "../../assets/backg-img-3-min.jpg";
 import { styled } from "styled-components";
 
 export const WelcomeSection = styled.section`
@@ -10,4 +10,48 @@ export const WelcomeSection = styled.section`
   background-position: right bottom;
   background-size: cover;
   background-attachment: fixed;
+  z-index: -1;
+`;
+export const Shadow = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.7);
+`;
+
+export const Title = styled.h1`
+  font-weight: 900;
+  font-size: 164px;
+  letter-spacing: 1.2;
+  color: ${(p) => p.theme.color.white};
+  text-align: center;
+  margin-bottom: 60px;
+`;
+
+export const ImgWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 80px;
+`;
+
+export const ImgFirst = styled.img`
+  position: relative;
+  height: 320px;
+  width: 600px;
+  border-radius: 150px;
+  border: 12px solid ${(p) => p.theme.color.grey};
+  overflow: hidden;
+
+  opacity: 1;
+  object-fit: cover;
+  transform: scale(1);
+  transition: transform 500ms ease-in-out, object-fit 500ms ease-in-out;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+  &.contain-card {
+    object-fit: contain;
+  }
 `;
